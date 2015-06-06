998335b8a5fece96f8935df0d0071329780d7515
HsuTing <hsuting0106@gmail.com>
2015-06-07 05:05:23 +0800

    add input num

--- python/course_selection/course_selection.py
+++ python/course_selection/course_selection.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 import csv
 import json
 
@@ -5,19 +6,36 @@ def Course_selection(csvFile, jsonFile, key_name, key_num, answer):
 	fcsv = open(csvFile, "r")
 	fjson = open(jsonFile, "r")
 	filterArray = []
+	filterNum = []
 
 	csvContent = csv.reader(fcsv)
 	for row in csvContent:
 		for i in range(0, len(key_num)):
 			if(row[ key_num[i] ] == key_name[i]):
 				filterArray.append(row)
-
-	print filterArray
+				print row
 	print "---------------------------------"
+
 	jsonContent = json.load(fjson)
-	for i in range(0, len(jsonContent["children"])):
+	while(i < len(jsonContent["children"])):
+		print jsonContent["children"][i]["name"] + "(total is " + str(len(filterArray)) + "):"
+
+		temp = []
+		tempCount = 0
 		for j in range(0, len(jsonContent["children"][i]["children"])):
-			print jsonContent["children"][i]["children"][j]["name"]
+			tempNum = raw_input(">> num of " + jsonContent["children"][i]["children"][j]["name"].encode("utf-8").strip() + " : ")
+			temp.append(tempNum)
+			tempCount = tempCount + int(tempNum)
+
+		if(tempCount != len(filterArray)):
+			print "The num of this part is not equal to total num, please input again"
+			print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+			continue
+
+		filterNum.append(temp)
+		i = i + 1
+		print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	print filterNum
 
 	fcsv.close()
 	fjson.close()
