d7fc9ce4fc824d5cef93d6cdd3a61f83158116d7
HsuTing <hsuting0106@gmail.com>
2015-06-15 16:55:07 +0800

    rm data

--- python/data/getdata.py
+++
@@ -1,9 +0,0 @@
-import urllib2
-
-"""get data from web"""
-def fromweb(url, name):
-	for i in range(0, len(url)):
-		content = urllib2.urlopen(url[i])
-		data = content.read()
-		fout = open(name[i], 'w+')
-		fout.write(data)
